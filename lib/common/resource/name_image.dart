d35697ab91f68f393b6a0fa8c45030b60071561c
Mai Tùng <kaitoshion724@gmail.com>
2022-04-13 09:27:26 +0700

    RevertStringAndImage

--- lib/common/resource/name_image.dart
+++ lib/common/resource/name_image.dart
@@ -1,11 +1,7 @@
 ///TODO: image png
 const png_ic_google = "assets/icons/png/ic_google.png";
-const png_ic_an_phu = "assets/icons/png/ic_anphu.png";
-const png_logo_an_phu = "assets/images/png/logo_an_phu.png";
 const png_ic_logo =  "assets/icons/png/ic_logo.png";
 const png_image_login = "assets/icons/png/png_image_login.png";
-const png_ic_lay = "assets/images/png/icon_lay.png";
-const png_ic_huawell ="assets/images/png/icon_huawell.png";
 const png_ic_success = "assets/images/png/ic_success.png";
 const png_notification = "assets/icons/png/bg_notification.png";
 const png_background_login = "assets/icons/png/png_background_login.png";
@@ -20,13 +16,9 @@ const svg_ic_recruitment = "assets/icons/svg/icon_recruitment.svg";
 const ic_svg_ap = "assets/icons/svg/ic_ap.svg";
 const ic_svg_logo = "assets/icons/svg/svg_ic_logo_toprate.svg";
 
-const ic_salary = "assets/icons/svg/ic_salary.svg";
+
 const ic_calendar = "assets/icons/svg/ic_calendar.svg";
 const ic_notification = "assets/icons/svg/ic_notification.svg";
-const ic_account_circle = "assets/icons/svg/ic_account_circle.svg";
-const ic_phone = "assets/icons/svg/ic_phone.svg";
-const ic_circle_calendar = "assets/icons/svg/ic_circle_calendar.svg";
-const ic_location = "assets/icons/svg/ic_location.svg";
 const ic_check_mark = "assets/icons/svg/ic_check_mark.svg";
 const ic_error = "assets/icons/svg/ic_error.svg";
 const ic_daily = "assets/icons/svg/ic_daily.svg";
