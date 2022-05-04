d5843d394f0861d38737cd62b448c9636353fdb9
Tung <kaitoshion724@gmail.com>
2022-05-04 11:29:03 +0700

    RemoveSplash

--- lib/blocs/splash/splash_bloc.dart
+++ lib/blocs/splash/splash_bloc.dart
@@ -9,16 +9,11 @@ import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:openapi/openapi.dart';
 import 'package:toprate_hrm/blocs/splash/splash_event.dart';
-import 'package:video_player/video_player.dart';
 
 class SplashBloc extends Bloc<SplashEvent, BaseState> {
   SplashBloc() : super(SplashInitial()) {
-    on<GetVideoEvent>((event,emit){
-      controller.play();
-      emit(GetVideoState());
-    });
+
   }
-  late VideoPlayerController controller;
   // saveAccountInformation(OWhoAmI? user) async {
   //   return PreferenceUtils.saveAccountInformation(user);
   // }
